79f68808597e254f8ae0fc0ec793dc9cb74c3f24
Matheus Benatti <matheus.cwb2@gmail.com>
2024-04-09 14:26:24 -0300

    Adição classificação por idade e sexo

--- src/ClassificacaoImcEnum.php
+++ src/ClassificacaoImcEnum.php
@@ -8,24 +8,33 @@ enum ClassificacaoImcEnum: string{
     case ObesidadeGrauII = 'Obesidade Grau II';
     case ObesidadeGrauIII = 'Obesidade Grau III';
     
-    public static function classifica(float $imc): string{
-        if ($imc>=40){
-            return 'Obesidade grau III';
+    public static function classifica(float $imc, SexoEnum $sexo, integer $idade): string{
+        if ($idade > 20){    
+            if ($imc>=40){
+                return 'Obesidade grau III';
+            }
+            if ($imc>=35 && $imc<40){
+                return 'Obesidade grau II';
+            }
+            if ($imc>=30 && $imc<35){
+                return 'Obesidade grau I';
+            }
+            if ($imc>=25 && $imc<30){
+                return 'Sobrepeso';
+            }
+            if ($imc>=18.5 && $imc<25){
+                return 'Normal';
+            }
+            if ($imc<18.5){
+                return 'Magreza';
+            }
         }
-        if ($imc>=35 && $imc<40){
-            return 'Obesidade grau II';
-        }
-        if ($imc>=30 && $imc<35){
-            return 'Obesidade grau I';
-        }
-        if ($imc>=25 && $imc<30){
-            return 'Sobrepeso';
-        }
-        if ($imc>=18.5 && $imc<25){
-            return 'Normal';
-        }
-        if ($imc<18.5){
-            return 'Magreza';
+        if ($sexo == "Masculino") {
+            if($idade == 10){
+                if ($imc) {
+                    # code...
+                }
+            }
         }
     }
 }
